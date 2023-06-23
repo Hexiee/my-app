92ef6dea3ee136060bea20ae2eef6dae8b5b16a4
Hexiee <136283192+Hexiee@users.noreply.github.com>
2023-06-22 22:11:13 -0300

    SKERE

---
+++ src/data.jsx
@@ -0,0 +1,39 @@
+export const data = [
+    {
+      id: 1,
+      img: 'https://images.unsplash.com/photo-1577221084712-45b0445d2b00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=698&q=80',
+      nameEjercicio: 'Zapatos Nike',
+      descripcion: 'Descripción del ejercicio 1',
+      repeticiones: 10,
+    },
+    {
+      id: 2,
+      img: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80',
+      nameEjercicio: 'Reloj',
+      descripcion: 'Descripción del ejercicio 2',
+      repeticiones: 8,
+    },
+    {
+      id: 3,
+      img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+      nameEjercicio: 'Smartwatch',
+      descripcion: 'Descripción del ejercicio 3',
+      repeticiones: 12,
+    },
+    {
+      id: 4,
+      img: 'https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+      nameEjercicio: 'Perfume',
+      descripcion: 'Descripción del ejercicio 4',
+      repeticiones: 15,
+    },
+    {
+      id: 5,
+      img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      nameEjercicio: 'Audífonos',
+      descripcion: 'Descripción del ejercicio 5',
+      repeticiones: 20,
+    },
+    // Aquí puedes agregar más ejercicios o modificar los existentes
+  ];
+  
